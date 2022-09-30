290a4204e2db4260e181935b507c3d4419485aff
Bompick <r.a.bayramov@gmail.com>
2022-09-30 14:37:16 +0300

    2

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,8 @@
 from .pages.main_page import MainPage
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
+    url = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, url)
     page.open()
     page.go_to_login_page()
 
