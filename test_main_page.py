27c49aa0e6e62687680febfc6048354823fceb80
Bompick <r.a.bayramov@gmail.com>
2022-10-01 22:29:51 +0300

    2022_10_01_evening

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,12 @@
 from .pages.main_page import MainPage
 
+
 def test_guest_can_go_to_login_page(browser):
     url = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, url)
     page.open()
     page.should_be_login_link()
+   
     
 
    
