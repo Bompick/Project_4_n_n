979b3e6069d28e9357adc162bebd019a9da0be52
Bompick <r.a.bayramov@gmail.com>
2022-10-04 15:46:17 +0300

    4_2_8_commit

--- pages/main_page.py
+++ pages/main_page.py
@@ -5,7 +5,7 @@ from .locators import MainPageLocators
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        self.browser.find_element(By.CSS_SELECTOR, "#registration_link").click()
+        self.browser.find_element(*MainPageLocators.LOGIN_LINK).click()
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
