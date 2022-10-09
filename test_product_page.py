617c1abdb7904e1f7cc82ca80ba7fffcaeb4f934
Bompick <r.a.bayramov@gmail.com>
2022-10-09 18:02:57 +0300

    4_3_3_commit

--- test_product_page.py
+++ test_product_page.py
@@ -2,8 +2,8 @@ from .pages.product_page import ProductPage
 import time
 
 def test_1(browser):
-    url = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    product_page = ProductPage(browser, url)# initialitiationof of object belonging to class ProductPage
+    url = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    product_page = ProductPage(browser, url)# initialitiation of of object belonging to class ProductPage
     product_page.open()
     product_page.add_to_basket()    
     product_page.solve_quiz_and_get_code()
